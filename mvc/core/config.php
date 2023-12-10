a5c557b626076c46b193473681f50bc4a48c9198
Võ Thanh Tú <77402349+Anhtraihoa@users.noreply.github.com>
2023-12-10 15:40:40 +0700

    Update config.php

--- mvc/core/config.php
+++ mvc/core/config.php
@@ -1,5 +1,5 @@
 <?php
-    define("base","http://localhost:8080/web_quan_ao/");
+    define("base","kely-server.mysql.database.azure.com");
     // đường dẫn đến thư mục chứa hình ảnh sản phẩm
     define("urlFileProduct","public/images/img_product/");
 ?>
