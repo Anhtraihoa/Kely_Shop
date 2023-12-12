2aa1305fb22fbc45ce2a26d3f46079c3ed22ff11
Võ Thanh Tú <77402349+Anhtraihoa@users.noreply.github.com>
2023-12-12 17:02:19 +0700

    Update config.php

--- mvc/core/config.php
+++ mvc/core/config.php
@@ -1,5 +1,5 @@
 <?php
-    define("base","https://kely.azurewebsites.net/Kely_Shop/");
+    define("base","https://kely.azurewebsites.net");
     // đường dẫn đến thư mục chứa hình ảnh sản phẩm
     define("urlFileProduct","/public/images/img_product/");
 ?>
