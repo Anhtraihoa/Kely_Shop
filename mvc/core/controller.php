378f7bfb4b8949e0f5e65c679cd3590455dbdde7
Võ Thanh Tú <77402349+Anhtraihoa@users.noreply.github.com>
2023-12-12 16:28:54 +0700

    Update controller.php

--- mvc/core/controller.php
+++ mvc/core/controller.php
@@ -1,27 +1,25 @@
-<?php
-    class controller{
+class controller {
 
-        function ModelAdmin($model){
-            require_once "./mvc/models/admin/".$model.".php";
-            return new $model;
-        }
-
-        function ViewAdmin($view, $data = []){
-            require_once "./mvc/views/admin/cpanel/".$view.".php";
-        }
-
-        function ModelClient($model){
-            require_once "./mvc/models/client/".$model.".php";
-            return new $model;
-        }
-
-        function ViewClinet($view, $data = []){
-            require_once "./mvc/views/client/cpanel/".$view.".php";
-        }
-
-        function ModelCommon($model){
-            require_once "./mvc/models/common/".$model.".php";
-            return new $model;
-        }
+    function ModelAdmin($model){
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/mvc/models/admin/".$model.".php";
+        return new $model;
     }
-?>
+
+    function ViewAdmin($view, $data = []){
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/mvc/views/admin/cpanel/".$view.".php";
+    }
+
+    function ModelClient($model){
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/mvc/models/client/".$model.".php";
+        return new $model;
+    }
+
+    function ViewClient($view, $data = []){
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/mvc/views/client/cpanel/".$view.".php";
+    }
+
+    function ModelCommon($model){
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/mvc/models/common/".$model.".php";
+        return new $model;
+    }
+}
